72f4aa6e30bcbfc567276bb6b21afabd6281ba39
Geremia Taglialatela <tagliala.dev@gmail.com>
2016-01-14 16:42:46 +0100

    Make rubocop happier

--- lib/route_translator/extensions/route_set.rb
+++ lib/route_translator/extensions/route_set.rb
@@ -4,8 +4,11 @@ module ActionDispatch
   module Routing
     class RouteSet
       def add_localized_route(mapping, path_ast, name, anchor, scope, path, controller, default_action, to, via, formatted, options_constraints, options)
-        RouteTranslator::Translator.translations_for(scope, self, name, path, controller, default_action, to, via, formatted, options_constraints, anchor, options) do |*translated_args|
-          add_route(*translated_args)
+        RouteTranslator::Translator.translations_for(self, path, name, options) do |translated_name, translated_path, translated_options|
+          translated_path_ast = ::ActionDispatch::Journey::Parser.parse(translated_path)
+          translated_mapping  = ::ActionDispatch::Routing::Mapper::Mapping.build(scope, self, translated_path_ast, controller, default_action, to, via, formatted, options_constraints, anchor, translated_options)
+
+          add_route translated_mapping, translated_path_ast, translated_name, anchor
         end
 
         if RouteTranslator.config.generate_unnamed_unlocalized_routes
--- lib/route_translator/translator.rb
+++ lib/route_translator/translator.rb
@@ -38,7 +38,7 @@ module RouteTranslator
       end
     end
 
-    def self.translations_for(scope, route_set, name, path, controller, default_action, to, via, formatted, options_constraints, anchor, options, &block)
+    def self.translations_for(route_set, path, name, options, &block)
       add_untranslated_helpers_to_controllers_and_views name, route_set.named_routes
 
       available_locales.each do |locale|
@@ -49,20 +49,17 @@ module RouteTranslator
           next
         end
 
-        translated_path_ast = ::ActionDispatch::Journey::Parser.parse(translated_path)
-        traslated_options = options.dup
+        translated_options = options.dup
 
-        unless traslated_options.include?(RouteTranslator.locale_param_key)
-          traslated_options.merge! RouteTranslator.locale_param_key => locale.to_s.gsub('native_', '')
+        unless translated_options.include?(RouteTranslator.locale_param_key)
+          translated_options.merge! RouteTranslator.locale_param_key => locale.to_s.gsub('native_', '')
         end
 
         translated_name = translate_name(name, locale)
         # TODO: Investigate this :(
         translated_name = nil if translated_name && route_set.named_routes.send(:routes)[translated_name.to_sym]
 
-        translated_mapping = ::ActionDispatch::Routing::Mapper::Mapping.build(scope, route_set, translated_path_ast, controller, default_action, to, via, formatted, options_constraints, anchor, traslated_options)
-
-        block.call translated_mapping, translated_path_ast, translated_name, anchor
+        block.call translated_name, translated_path, translated_options
       end
     end
 
